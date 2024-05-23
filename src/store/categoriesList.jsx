f15a5d290d4edb0c7300cd64037c5bdf4bbc659d
Hyunssun <sunny20829@gmail.com>
2024-05-23 09:58:05 +0900

    recoil-persist

--- src/store/categoriesList.jsx
+++ src/store/categoriesList.jsx
@@ -1,6 +1,10 @@
 import { atom } from "recoil";
 
+import { recoilPersist } from "recoil-persist";
+const { persistAtom } = recoilPersist();
+
 export const categoriesList = atom({
   key: "categoriesList",
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
--- src/store/goPath.jsx
+++ src/store/goPath.jsx
@@ -1,6 +1,10 @@
 import { atom } from "recoil";
 
+import { recoilPersist } from "recoil-persist";
+const { persistAtom } = recoilPersist();
+
 export const goPath = atom({
   key: "goPath",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
