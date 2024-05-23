549b415881242ed1f804026114730881f2953850
Hyunssun <sunny20829@gmail.com>
2024-05-23 17:32:51 +0900

    modify dateCalc

--- src/utill/date.js
+++ src/utill/date.js
@@ -4,15 +4,18 @@ export const dateCalc = (date) => {
   const koreaTimeDiff = 9 * 60 * 60 * 1000;
   const korNow = new Date(utc + koreaTimeDiff);
 
-  if (date === "today") {
-    return `${korNow.getFullYear()}-${korNow.getMonth()}-${korNow.getDate()}`;
-  } else if (date === "day7") {
-    return new Date(korNow.setDate(korNow.getDate() - 7));
-  } else if (date === "month1") {
-    return new Date(korNow.setMonth(korNow.getMonth() - 1));
-  } else if (date === "month3") {
-    return new Date(korNow.setMonth(korNow.getMonth() - 3));
-  } else if (date === "year1") {
-    return new Date(korNow.setFullYear(korNow.getFullYear() - 1));
+  switch (date) {
+    case "today":
+      return `${korNow.getFullYear()}-${
+        korNow.getMonth() + 1
+      }-${korNow.getDate()}`;
+    case "day7":
+      return new Date(korNow.setDate(korNow.getDate() - 7));
+    case "month1":
+      return new Date(korNow.setMonth(korNow.getMonth() - 1));
+    case "month3":
+      return new Date(korNow.setMonth(korNow.getMonth() - 3));
+    case "year1":
+      return new Date(korNow.setFullYear(korNow.getFullYear() - 1));
   }
 };
